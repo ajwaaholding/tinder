641a8ce794d9661f054d3f8f27f8ba7ac6ef452c
ismailkhan <ajwaaholding@gmail.com>
2025-06-02 09:13:34 +0300

    User Schema Creations

--- src/app.js
+++ src/app.js
@@ -3,13 +3,87 @@ const connectDB = require("./config/database"); // also fixed the path (should b
 const app = express();
 const User = require("./models/user");
 //Connecting To Server
+
 app.use(express.json());
+
+//Register API
 app.post("/signup", async (req, res, next) => {
   const data = req.body;
 
   const user = new User(data);
   await user.save();
   res.send("User Registered Sucessfuly");
+});
+
+//GET API for one
+app.get("/users", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const data = await User.findById(userId);
+    if (!data) {
+      res.send("No User Found");
+    } else {
+      res.send(data);
+    }
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+//Get All Users
+app.get("/feeds", async (req, res) => {
+  try {
+    const data = await User.find({});
+    if (!data.length) {
+      res.send("No Users Found");
+    } else {
+      res.send(data);
+    }
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Delete by ID
+app.delete("/users", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.userId);
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+app.patch("/users", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const data = req.body;
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    res.send(user);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+app.patch("/users", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const data = req.body;
+    console.log(data, "User data");
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    res.send(user);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(400).send(err.message, "Error Occured");
+  }
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
