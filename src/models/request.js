08e29e6ab04fc09d6916eca8520d4e1b6a78d630
ismailkhan <ajwaaholding@gmail.com>
2025-06-17 09:58:51 +0300

    created index to optimise

--- src/models/request.js
+++ src/models/request.js
@@ -20,6 +20,16 @@ const RequestSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+// RequestSchema.pre("save", function (next) {
+//   const connectionRequest = this;
+//   if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+//     return next(new Error("Request cannot be sent!!"));
+//   }
+//   next();
+// });
 
+//indexing
+
+RequestSchema.index({ fromUserId: 1, toUserId: 1 });
 const ConnectionRequest = mongoose.model("connectionRequest", RequestSchema);
 module.exports = ConnectionRequest;
